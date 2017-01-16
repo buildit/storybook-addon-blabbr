0b24f0db3303c5a6e5041240b39a791ee329014c
Panos Voudouris <panosvoudouris@gmail.com>
2017-01-16 15:49:45 +0000

    Create panel

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import addons from '@kadira/storybook-addons';
--- src/register.jsx
+++ src/register.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import addons from '@kadira/storybook-addons';
 import Panel from './components/Panel';
 
-addons.register('buildit/storybook-addon-reviewer', (api) => {
+addons.register('buildit/reviewer', (api) => {
   const channel = addons.getChannel();
 
-  addons.addPanel('buildit/storybook-addon-reviewer', {
+  addons.addPanel('buildit/reviewer', {
     title: 'Reviewer',
     render: () => <Panel channel={channel} api={api} key="knobs-panel" />,
   });
