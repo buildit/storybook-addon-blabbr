c74d2be616827aba9706e5cb01f0aaef9e51affb
Leanne Tite <leanne.tite@wipro.com>
2017-01-19 13:11:08 +0000

    Keep stub data

---
+++ stub/comments.Button.js
@@ -0,0 +1,24 @@
+const stubComments = () => ({
+	"success": 1,
+	"comments": [
+		{
+			"id": "-KalyE-fFQifQwUpTrRl",
+			"comment": "New update added",
+			"componentId": "Button",
+			"timestamp": 1484749598756,
+			"userEmail": "jack.smith@wipro.com",
+			"userName": "Jack",
+			"version": "0.0.1"
+		},
+		{
+			"id": "-Kalz6AsIqNyH6zc-20X",
+			"comment": "New update added",
+			"componentId": "Button",
+			"timestamp": 1484749828849,
+			"userEmail": "jack.smith@wipro.com",
+			"userName": "Jack",
+			"version": "0.0.1"
+		}
+	]
+});
+export default stubComments;
