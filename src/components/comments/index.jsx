456c21c52ec3bc459abc2372f5a0f1c190b4f652
Jonathan Ellis-Craig <jonathan.craig@wipro.com>
2017-01-25 14:49:38 +0000

    animation added in

--- src/components/comments/index.jsx
+++ src/components/comments/index.jsx
@@ -3,6 +3,8 @@ import { ListGroup, Button } from 'react-bootstrap';
 import Comment from '../comment';
 import SubmitComment from '../submitComment';
 import { getTimestamp } from '../../utils';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import './styles.css';
 
 const Comments = ({
 	comments,
@@ -18,43 +20,44 @@ const Comments = ({
 	isShowingAllComments
 }) => {
 	const commentsComponents = comments.map((comment, i) => {
-		const timestamp = getTimestamp(comment.timestamp);
-		const lastUpdated = getTimestamp(comment.lastUpdated);
-		const beingEditted = comment.id === commentIdBeingEditted;
+			const timestamp = getTimestamp(comment.timestamp);
+			const lastUpdated = getTimestamp(comment.lastUpdated);
+			const beingEditted = comment.id === commentIdBeingEditted;
 
-		if (beingEditted) {
-			userCommentBeingUpdated = userCommentBeingUpdated === null ? comment.comment : userCommentBeingUpdated;
-		}
+			if (beingEditted) {
+				userCommentBeingUpdated = userCommentBeingUpdated === null ? comment.comment : userCommentBeingUpdated;
+			}
 
-		return (
-			<div key={i}>
-			{ !!beingEditted === true ?
-				<SubmitComment key={i}
-					userComment={userCommentBeingUpdated}
-					onUserCommentChange={onUserCommentUpdate}
-					onCommentSubmit={onUserCommentEditSave}
-					onCommentCancel={onUserCommentEditCancel}
-					title={'Edit comment'}
-					comment={comment}
-					type={'Edit'}
-				/>
-				 :
-				 <Comment
-					onUserCommentEdit={onUserCommentEdit}
-					onUserCommentDelete={onUserCommentDelete}
-					currentUserIsOwner={currentUser === comment.userEmail}
-					username={comment.userName}
-					emailId={comment.userEmail}
-					timestamp={timestamp}
-					comment={comment.comment}
-					commentId={comment.id}
-					edited={comment.edited}
-					lastUpdated={lastUpdated}
- 		  	 />
-			 }
-			</div>
-		)
-	});
+			return (
+				<div key={comment.id} className="comment-container">
+				 { !!beingEditted === true ?
+					<SubmitComment
+						userComment={userCommentBeingUpdated}
+						onUserCommentChange={onUserCommentUpdate}
+						onCommentSubmit={onUserCommentEditSave}
+						onCommentCancel={onUserCommentEditCancel}
+						title={'Edit comment'}
+						comment={comment}
+						type={'Edit'}
+					/>
+					 :
+					 <Comment
+						onUserCommentEdit={onUserCommentEdit}
+						onUserCommentDelete={onUserCommentDelete}
+						currentUserIsOwner={currentUser === comment.userEmail}
+						username={comment.userName}
+						emailId={comment.userEmail}
+						timestamp={timestamp}
+						comment={comment.comment}
+						commentId={comment.id}
+						edited={comment.edited}
+						lastUpdated={lastUpdated}
+	 		  	 />
+				 }
+				</div>
+			)
+		});
+
 	const showAllCommentsLink = !isShowingAllComments ?
 		<Button bsStyle="link" block style={{ marginBottom: 20 }} onClick={onShowAllComments}>
 			Show all comments
@@ -63,9 +66,17 @@ const Comments = ({
 
 	return 	(
 		<div>
-			<ListGroup componentClass="ul">
-				{ commentsComponents.length ? commentsComponents : <p>No comments to show for this story</p> }
-			</ListGroup>
+			<ReactCSSTransitionGroup
+				transitionName="comment"
+				transitionEnterTimeout={500}
+				transitionLeaveTimeout={500}
+				transitionAppear={false}
+			>
+				{ comments.length ?
+					commentsComponents :
+					<p key="no-comments">No comments to show for this story</p>
+				}
+			</ReactCSSTransitionGroup>
 			{showAllCommentsLink}
 		</div>
 	);
