761faa5e00469472c2a010ebf03900f737cd8529
Panos Voudouris <panosvoudouris@gmail.com>
2017-01-26 14:32:41 +0000

    Do not check in db config

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  apiKey: 'AIzaSyCZ-edNRbLIigWheJTmqwXpGUXFPabF5M0',
-  authDomain: 'comments-c8e4c.firebaseapp.com',
-  databaseURL: 'https://comments-c8e4c.firebaseio.com',
-  storageBucket: 'comments-c8e4c.appspot.com',
-  messagingSenderId: '574115914803',
-};
-
-export default config;
