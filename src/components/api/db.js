a3dc7875252e37f997887e3e6855b5c84ceb0036
Leanne Tite <leanne.tite@wipro.com>
2017-01-26 11:44:32 +0000

    Moving config dir inside src dir so webpack sees it

--- src/components/api/db.js
+++ src/components/api/db.js
@@ -1,6 +1,6 @@
 // Initialize Firebase
 import * as firebase from 'firebase';
-import config from '../../../config/db';
+import config from '../../config/db';
 
 firebase.initializeApp(config);
 const db = firebase.database();
