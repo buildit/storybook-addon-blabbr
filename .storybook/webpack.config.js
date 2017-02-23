53df42c80d286e22833142afb0da5a815fdd36e8
Panos Voudouris <panosvoudouris@gmail.com>
2017-02-23 12:49:57 +0000

    Cleanup dependencies

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -9,8 +9,7 @@ module.exports = {
 		loaders: [
 			{ test: /\.(js|jsx)$/, exclude: [ /node_modules/ ], loader: 'babel' },
 			{ test: /\.(png|jpg|gif|svg)$/, loader: 'file-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-			{ test: /\.(eot|ttf|svg|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' }
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
 		]
 	},
 
