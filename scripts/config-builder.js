ca12b0ac39bbd0ddfe58a91b30532bc15ff90526
Panos Voudouris <panosvoudouris@gmail.com>
2017-05-26 12:56:50 +0100

    Config builder

--- scripts/config-builder.js
+++ scripts/config-builder.js
@@ -13,8 +13,9 @@ const buildConfig = (local) => {
       const consumerDirectory = path.join(currentDir, local ? '.' : '../../..');
       const storybookDirectory = path.join(consumerDirectory, '.storybook');
       const blabbrConfig = 'blabbr-config.js';
-      const head = 'head.sample.html';
+
       let exportsString = 'db';
+
       // Generate config
       const out = fs.createWriteStream(path.join(storybookDirectory, blabbrConfig));
       out.write('const db = {\n');
@@ -41,11 +42,37 @@ const buildConfig = (local) => {
         exportsString += ', version';
 
         // Copy the head
+        fs.access(path.join(storybookDirectory, 'head.html'), fs.constants.R_OK, (err) => {
+          const head = 'head.sample.html';
+          if (err) {
+            // If it does not exist just copy over
+            fs.copy(path.join(`${currentDir}`, 'config', head),
+                    path.join(`${storybookDirectory}`, 'head.html'));
+            return;
+          }
+          // If it does exist then copy aside and prompt
+          fs.copy(path.join(`${currentDir}`, 'config', head),
+                  path.join(`${storybookDirectory}`, head))
+            .then(() => {
+              console.log(chalk.bgBlue('Existing head.html found, copied the config into head.sample.html. You must now merge them.'));
+            })
+            .catch(() => console.log(chalk.red(`Could not copy
+              ${path.join(`${currentDir}`, 'config', head)} into ${storybookDirectory}`)));
+        });
         // Copy a versions file
+        fs.access(path.join(storybookDirectory, 'versions.json'), fs.constants.R_OK, (err) => {
+          const versions = 'versions.sample.json';
+          if (err) {
+            // If it does not exist just copy over
+            fs.copy(path.join(`${currentDir}`, 'config', versions),
+                    path.join(`${storybookDirectory}`, 'versions.json'));
+          }
+        });
       }
 
       out.write(`export { ${exportsString} };\n`);
       out.end();
+      console.log(chalk.bgBlue(`Config created in ${storybookDirectory}`));
     } else if (answers.configureBlabbr) {
       // Just copy the samlpe files
       copyFiles(local);
