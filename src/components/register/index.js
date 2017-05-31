5ba61292d8b552bfd2f635af6bd17967f01eca84
Tom Robinson <tom.robinson@wipro.com>
2017-05-31 13:32:13 +0100

    Removed unnecessary commented code

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -67,55 +67,6 @@ class Register extends Component {
   }
 }
 
-/*const Register = ({
-  onUserNameChange,
-  onUserEmailChange,
-  onRegisterSubmit,
-  userName,
-  userEmail,
-}) => {
-  const formTitle = (
-    <h2>Register to add comments</h2>
-  );
-
-  return (
-    <section className="blabbr-register">
-      {formTitle}
-      <form>
-        <div>
-          <label htmlFor="blabbr-userName">
-            Display name:
-          </label>
-          <input
-            id="blabbr-userName"
-            type="text"
-            value={userName || ''}
-            onChange={onUserNameChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="blabbr-email">
-            Email:
-          </label>
-          <input
-            id="blabbr-email"
-            type="text"
-            value={userEmail || ''}
-            onChange={onUserEmailChange}
-          />
-        </div>
-        <button
-          type="submit"
-          onClick={onRegisterSubmit}
-        >
-          Register
-        </button>
-      </form>
-    </section>
-  );
-};*/
-
-
 Register.propTypes = {
   onUserNameChange: PropTypes.func.isRequired,
   onUserEmailChange: PropTypes.func.isRequired,
