59a178b2d86c185530b846a93a46478b895c3e60
Panos Voudouris <panosvoudouris@gmail.com>
2017-05-31 12:56:11 +0100

    Remove head references, add version dropdown

--- src/components/storyWrapper/index.js
+++ src/components/storyWrapper/index.js
@@ -1,52 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Versions from '../versions';
-import { version } from '../../utils/config';
+import './styles.css';
 
 class StoryWrapper extends React.Component {
   constructor(props) {
     super(props);
-    this.showComments = this.showComments.bind(this);
-
     this.state = {
       commentCount: 0,
     };
   }
 
-  showComments() {
-    this.props.channel.emit('blabbrFocusTab');
-  }
-
   render() {
-    const versionDropdown = version && <Versions activeVersion={version} />;
-
     return (
-      <div>
-        { versionDropdown }
-        {this.props.children}
+      <div id="blabbr-storyWrapper">
+        <Versions />
+        <div id="blabbr-storyWrapper-content">
+          {this.props.children}
+        </div>
       </div>
     );
-    // Reenable this when we have a valid style
-        // <button
-        //   type="button"
-        //   className="btn btn-default"
-        //   onClick={this.showComments}
-        //   style={{
-        //     position: 'fixed',
-        //     top: '0',
-        //     right: '0',
-        //     display: 'block',
-        //   }}
-        // >
-        //   <span className="glyphicon glyphicon-comment" />
-        // </button>
-    //   </div>
-    // );
   }
 }
 
 StoryWrapper.propTypes = {
-  channel: PropTypes.object.isRequired,
   children: PropTypes.node.isRequired,
 };
 
--- src/components/versions/index.js
+++ src/components/versions/index.js
@@ -1,20 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { version } from '../../utils/config';
 import './styles.css';
 
-const Versions = (currentVersion) => {
-  return (
-    <div id="blabbr-version-history">
-      <div class="dropdown">
-        <button class="dropbtn" id="blabbr-version-history-dropbtn">Versions</button>
-        <div class="dropdown-content" id="blabbr-version-history-dropdown-content">
+class Versions extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      versions: null,
+    };
+  }
+
+  componentWillMount() {
+    if (version) {
+      const url = window.parent.location;
+      const location = `${url.protocol}//${url.hostname}:${url.port}/versions.json`;
+
+      fetch(location).then((response) => {
+        if (response.ok) {
+          response.json().then((data) => {
+            this.setState({
+              versions: data.reverse(),
+            });
+          });
+        }
+      }).catch(() => {
+        // Ignore. Maybe we want to remove the div?
+      });
+    }
+  }
+
+  render() {
+    if (version && !this.state.versions) {
+      return null;
+    }
+
+    const url = window.parent.location;
+
+    return (
+      <div id="blabbr-version-history">
+        <div className="dropdown">
+          <button className="dropbtn" >{version}</button>
+          <div className="dropdown-content">
+            {this.state.versions.map(v => (
+              <a
+                href={`${url.protocol}//${url.hostname}:${url.port}/${v}/${url.search}${url.hash}`}
+                target="_parent"
+              >
+                {v}
+              </a>))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-SubmitComment.propTypes = {
+Versions.propTypes = {
   onUserCommentChange: PropTypes.func.isRequired,
   onCommentSubmit: PropTypes.func.isRequired,
   userComment: PropTypes.string,
