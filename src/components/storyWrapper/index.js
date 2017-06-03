b481f7e8b21d83051c3adf65e234e2b35d9b4c5f
Panos Voudouris <panosvoudouris@gmail.com>
2017-06-03 22:34:00 +0100

    Move versions to props from state

--- src/components/storyWrapper/index.js
+++ src/components/storyWrapper/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Versions from '../versions';
+import { version } from '../../utils/config';
 import './styles.css';
 
 class StoryWrapper extends React.Component {
@@ -8,13 +9,33 @@ class StoryWrapper extends React.Component {
     super(props);
     this.state = {
       commentCount: 0,
+      versions: null
     };
+  }
+
+  componentWillMount() {
+    if (version) {
+      const url = window.parent.location;
+      const location = `${url.protocol}//${url.hostname}:${url.port}/versions.json`;
+
+      fetch(location).then((response) => {
+        if (response.ok) {
+          response.json().then((data) => {
+            this.setState({
+              versions: data.reverse(),
+            });
+          });
+        }
+      }).catch(() => {
+        // Ignore. Maybe we want to remove the div?
+      });
+    }
   }
 
   render() {
     return (
       <div id="blabbr-storyWrapper">
-        <Versions />
+        <Versions versions={this.state.versions}/>
         <div id="blabbr-storyWrapper-content">
           {this.props.children}
         </div>
--- src/components/versions/index.js
+++ src/components/versions/index.js
@@ -3,67 +3,35 @@ import PropTypes from 'prop-types';
 import { version } from '../../utils/config';
 import './styles.css';
 
-class Versions extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      versions: null,
-    };
+const Versions = ({
+  versions,
+}) => {
+  if (version && !versions) {
+    return null;
   }
 
-  componentWillMount() {
-    if (version) {
-      const url = window.parent.location;
-      const location = `${url.protocol}//${url.hostname}:${url.port}/versions.json`;
+  const url = window.parent.location;
 
-      fetch(location).then((response) => {
-        if (response.ok) {
-          response.json().then((data) => {
-            this.setState({
-              versions: data.reverse(),
-            });
-          });
-        }
-      }).catch(() => {
-        // Ignore. Maybe we want to remove the div?
-      });
-    }
-  }
-
-  render() {
-    if (version && !this.state.versions) {
-      return null;
-    }
-
-    const url = window.parent.location;
-
-    return (
-      <div id="blabbr-version-history">
-        <div className="dropdown">
-          <button className="dropbtn" >{version}</button>
-          <div className="dropdown-content">
-            {this.state.versions.map(v => (
-              <a
-                href={`${url.protocol}//${url.hostname}:${url.port}/${v}/${url.search}${url.hash}`}
-                target="_parent"
-              >
-                {v}
-              </a>))}
-          </div>
+  return (
+    <div id="blabbr-version-history">
+      <div className="dropdown">
+        <button className="dropbtn" >{version}</button>
+        <div className="dropdown-content">
+          {versions.map(v => (
+            <a
+              href={`${url.protocol}//${url.hostname}:${url.port}/${v}/${url.search}${url.hash}`}
+              target="_parent"
+            >
+              {v}
+            </a>))}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Versions.propTypes = {
-  onUserCommentChange: PropTypes.func.isRequired,
-  onCommentSubmit: PropTypes.func.isRequired,
-  userComment: PropTypes.string,
-  type: PropTypes.string,
-  comment: PropTypes.object,
-  onCommentCancel: PropTypes.func,
-  title: PropTypes.string,
+  versions: PropTypes.array.isRequired,
 };
 
 export default Versions;
