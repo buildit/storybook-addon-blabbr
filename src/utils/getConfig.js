1f43359ffd3cce4b29366acfde15a0a0b0c975da
Tom Robinson <tom.ro88o@gmail.com>
2017-06-16 14:39:31 +0100

    Fixed final lint issue

--- src/utils/getConfig.js
+++ src/utils/getConfig.js
@@ -8,9 +8,9 @@ const getConfig = () =>
       const url = window.parent.location;
       const location = `${url.protocol}//${url.hostname}:${url.port}/storybook-config.json`;
 
-      fetch(location).then((response) => {
+      fetch(location).then(response => {
         if (response.ok) {
-          response.json().then((data) => {
+          response.json().then(data => {
             if (data && data.storybook) {
               configFile = data.storybook;
               resolve(configFile);
